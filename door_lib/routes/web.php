9bf97dc238559b697e0533831b8ef9210edd6bdf
Re-ib75 <156016106+Re-ib75@users.noreply.github.com>
2024-02-27 13:13:24 +0300

    create content to book and create with controller ,model and route

--- door_lib/routes/web.php
+++ door_lib/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DoorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+   // return view('welcome');
+//});
+
+Route::get('/',[DoorController::class,'index']);
