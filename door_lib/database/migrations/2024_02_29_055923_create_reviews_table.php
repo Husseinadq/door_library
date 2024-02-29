33720a33bc714e945ba4e750a0eee1401cd8a0a8
ameer-alshuga <ameeralshojaa@gmail.com>
2024-02-29 09:38:39 +0300

    create tables (reviews, publisher, images)

---
+++ door_lib/database/migrations/2024_02_29_055923_create_reviews_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('book_id');
+            $table->double('rating');
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
