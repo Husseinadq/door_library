7228d08c47d077d4ad705c2a36ed20e5410ed007
ameer-alshuga <ameeralshojaa@gmail.com>
2024-02-29 13:22:29 +0300

    create  publisher functions (index, create ,store edit)

--- door_lib/app/Http/Controllers/backend/PublisherController.php
+++ door_lib/app/Http/Controllers/backend/PublisherController.php
@@ -4,8 +4,48 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Publisher;
+
 
 class PublisherController extends Controller
 {
-    //
+
+
+    public function index()
+    {
+
+        $publishers=Publisher::all();
+        return view('backend.publisher.index',compact('publishers'));   
+    }
+    
+   public function create(){
+    return view('backend.publisher.create');
+   } 
+
+
+
+
+   public function store(Request $request){
+
+     $publisher=new Publisher;
+     $publisher->name=$request->pub_name;
+     $publisher->pub_websit=$request->input_websit;
+     $publisher->decripton=$request->pub_dec;
+     $publisher->icon='';
+     $publisher->save();
+     return redirect()->route('publisher.index');  // Replace 'publishers.index' with your actual route name
+
+      
+
+
+
+   } 
+
+   public function edit(){
+    return view('backend.publisher.edit');
+   } 
+
+
+
+
 }
--- door_lib/routes/web.php
+++ door_lib/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoorController;
 use App\Http\Controllers\backend\AuthorController;
+use App\Http\Controllers\backend\PublisherController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +23,9 @@ use App\Http\Controllers\backend\AuthorController;
 
 Route::get('/',[DoorController::class,'index']);
 Route::get('author/index', [AuthorController::class, 'index'])->name('author.index');
+
+
+Route::get('/publisher/index',[PublisherController::class,'index'])->name('publisher.index');
+Route::get('/publisher/create',[PublisherController::class,'create'])->name('publisher.create');
+Route::get('/publisher/edit',[PublisherController::class,'edit'])->name('publisher.edit');
+Route::post('/publisher/store',[PublisherController::class,'store'])->name('publisher.store');
